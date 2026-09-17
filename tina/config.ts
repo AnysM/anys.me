bdf6661e0943f907845b21503e39183f4fb748fa
Anys Mechkar <anys@anys.me>
2026-09-17 17:46:38 +0000

    Tina: Client ID du projet integre dans la config (une variable d'env de moins)

--- tina/config.ts
+++ tina/config.ts
@@ -8,7 +8,7 @@ const branch =
 
 export default defineConfig({
   branch,
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "ae2b91fd-6332-4b65-98f1-ca0a428d27e3",
   token: process.env.TINA_TOKEN || "",
   build: { outputFolder: "admin", publicFolder: "public" },
   media: { tina: { mediaRoot: "img", publicFolder: "public" } },
